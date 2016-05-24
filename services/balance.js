3a6f3550ada637c4d19ba62e7dda21ac7502be17
d.tupalov <d.tupalov@rginvest.com>
2016-05-24 20:20:46 +0300

    # Привязал запрос на баланс к более быстрому сервису

--- services/balance.js
+++ services/balance.js
@@ -2,23 +2,33 @@
 
 const request = require('request');
 const isLoggedIn = require('./is_logged_in');
+const cheerio = require('cheerio');
 
 module.exports = function (session) {
     return new Promise(function (resolve, reject) {
 
+        let uri = 'https://private.15-58m11.ru/onyma/system/literpc/';
         let result = {
             "balance"  : 0.00,
             "isSuccess": false
         };
         let cookieJAR = request.jar();
-        cookieJAR.setCookie('onm_group=' + session.onm_group, 'https://private.15-58m11.ru/onyma/rm/party/' + session.dashboardURL);
-        cookieJAR.setCookie('onm_session=' + session.onm_session, 'https://private.15-58m11.ru/onyma/rm/party/' + session.dashboardURL);
+        cookieJAR.setCookie('onm_group=' + session.onm_group, uri);
+        cookieJAR.setCookie('onm_session=' + session.onm_session, uri);
 
         request({
-            method : 'GET',
-            baseUrl: 'https://private.15-58m11.ru/',
-            uri    : '/onyma/rm/party/' + session.dashboardURL,
-            jar    : cookieJAR
+            method: 'POST',
+            uri   : uri,
+            jar   : cookieJAR,
+            form  : {
+                body: JSON.stringify([{
+                    "id"     : 0,
+                    "package": "crms-balance-refresher",
+                    "fn"     : "refresh",
+                    "data"   : {}
+                }])
+            }
+
         }, (error, response, body) => {
 
             if (!isLoggedIn(response)) {
@@ -27,9 +37,11 @@ module.exports = function (session) {
                 return;
             }
 
-            let matched = body.match(/Баланс.+[\n\r\t\s]+?.+?>(.+?)</);
-            let balance = Number((parseFloat(matched[1])).toFixed(2));
-            result.balance = balance;
+            body = (JSON.parse(body)).result[0].result.result;
+            let $ = cheerio.load(body);
+            let balance = $($('.infoblock .value').get(2)).text();
+
+            result.balance = Number(balance);
             result.isSuccess = true;
             resolve(result);
         });

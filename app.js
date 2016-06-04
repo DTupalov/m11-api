0c958f680293c4c632b094cb2a4cfee9ffa6d4ff
d.tupalov <d.tupalov@rginvest.com>
2016-06-04 09:29:01 +0300

    Обработка непойманных ошибок, чтобы сервер не падал
    + Добавил в middleware обработку непойманных ошибок

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(function (res, req, next) {
+    process.on('uncaughtException', (err) => {
+        next(err);
+    });
+    next();
+});
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
